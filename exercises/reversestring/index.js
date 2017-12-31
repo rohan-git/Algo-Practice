e1ca527dc2cab0b6e0838c1186507fad14642ba4
rohan <1411081+rohan-git@users.noreply.github.com>
2017-12-31 18:48:20 -0500

    c1

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+  if(str.length == 1)
+    return str.charAt(0);
+  return
+    rev(str.subString(1))
+
+  return strRev;
+}
+
+function reverse1(str) {
+
+  //return str.split('').reverse().join('');
+  var strRev = '';
+
+  for(var x = 0; x < str.length; x++)
+  {
+    strRev+= str.charAt(str.length-1-x);
+  }
+
+  return strRev;
+}
+
 
 module.exports = reverse;
